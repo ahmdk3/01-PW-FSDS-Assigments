f4031ec274c53d89c08a6d4cb8b54f33262cd40e
ahmdk3 <ahamad.kedia+github@gmail.com>
2023-08-19 23:10:21 +0530

    1st Q&A

--- 01 19-Aug-23/01_Aug13.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-myList = np.array(range(10))
-print(myList)
-
-### Text Here ##
---
+++ 01 19-Aug-23/19Aug23.py
@@ -0,0 +1,31 @@
+
+#Basic variable based questions 
+#1. Declare two variables, `x` and `y`, and assign them integer values. Swap the values of these variables without using any temporary variable.
+x=5
+y=6
+print("Before Swap: x={}, y={}".format(x,y))
+x=x+y
+y=x-y
+x=x-y
+print("After Swap: x={}, y={}".format(x,y))
+
+#2. Create a program that calculates the area of a rectangle. Take the length and width as inputs from the user and store them in variables. Calculate and display the area.
+
+
+#3. Write a Python program that converts temperatures from Celsius to Fahrenheit. Take the temperature in Celsius as input, store it in a variable, convert it to Fahrenheit, and display the result.
+
+
+#String based questions 
+#1. Write a Python program that takes a string as input and prints the length of the string.
+
+
+#2. Create a program that takes a sentence from the user and counts the number of vowels (a, e, i, o, u) in the string.
+
+
+#3. Given a string, reverse the order of characters using string slicing and print the reversed string.
+
+
+#4. Write a program that takes a string as input and checks if it is a palindrome (reads the same forwards and backwards).
+
+
+#5. Create a program that takes a string as input and removes all the spaces from it. Print the modified string without spaces.
